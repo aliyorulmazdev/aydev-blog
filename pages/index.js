6cae42da3014cbd90c9b89e7a93b0eca1ce118e9
aliyorulmazdev <aliyorulmazdev@gmail.com>
2023-10-19 22:10:38 +0300

    Test Title

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,65 @@
+import Head from 'next/head';
 import { useState } from 'react';
-
 import { Row, Button } from 'react-bootstrap';
 import PageLayout from 'components/PageLayout';
 import AuthorIntro from 'components/AuthorIntro';
 import FilteringMenu from 'components/FilteringMenu';
-
 import { useGetBlogsPages } from 'actions/pagination';
 import { getPaginatedBlogs } from 'lib/api';
 
-export default function Home({blogs}) {
+export default function Home({ blogs }) {
   const [filter, setFilter] = useState({
     view: { list: 0 },
     date: { asc: 0 }
   });
+
+  const pageTitle = 'aydevBlog - Çarpıcı bir öğrenci yazılımcı sözü'; // Başlık metni
 
   const {
     pages,
     isLoadingMore,
     isReachingEnd,
     loadMore
-  } = useGetBlogsPages({blogs, filter});
+  } = useGetBlogsPages({ blogs, filter });
 
   return (
-    <PageLayout>
-      <AuthorIntro />
-      <FilteringMenu
-        filter={filter}
-        onChange={(option, value) =>
-          setFilter({...filter, [option]: value})
-        }
-      />
-      <hr/>
-      <Row className="mb-5">
-        {pages}
-      </Row>
-      <div style={{textAlign: 'center'}}>
-        <Button
-          onClick={loadMore}
-          disabled={isReachingEnd || isLoadingMore}
-          size="lg"
-          variant="outline-secondary">
-          {isLoadingMore ? '...' : isReachingEnd ? 'No more blogs' : 'More Blogs'}
-        </Button>
-      </div>
-    </PageLayout>
-  )
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        {/* Diğer meta etiketlerini buraya ekleyebilirsiniz */}
+      </Head>
+
+      <PageLayout>
+        <AuthorIntro />
+        <FilteringMenu
+          filter={filter}
+          onChange={(option, value) =>
+            setFilter({ ...filter, [option]: value })
+          }
+        />
+        <hr />
+        <Row className="mb-5">{pages}</Row>
+        <div style={{ textAlign: 'center' }}>
+          <Button
+            onClick={loadMore}
+            disabled={isReachingEnd || isLoadingMore}
+            size="lg"
+            variant="outline-secondary"
+          >
+            {isLoadingMore ? '...' : isReachingEnd ? 'No more blogs' : 'More Blogs'}
+          </Button>
+        </div>
+      </PageLayout>
+    </>
+  );
 }
 
 export async function getStaticProps() {
-  const blogs = await getPaginatedBlogs({offset: 0, date: 'desc'});
+  const blogs = await getPaginatedBlogs({ offset: 0, date: 'desc' });
   return {
     props: {
       blogs
     },
-    revalidate:1
-  }
+    revalidate: 1
+  };
 }
